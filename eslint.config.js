f79623c1c194b4cdf1d2172ed43d91024de52c1b
DiegoT4l <lopez.diiego.107@hotmail.com>
2025-02-18 00:45:55 -0600

    chore: Update ESLint configuration and add scripts for linting and formatting checks

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,6 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import pluginQuery from '@tanstack/eslint-plugin-query';
-import eslintConfigPrettier from 'eslint-config-prettier';
-const eslintPluginPrettierRecommended = import('eslint-plugin-prettier/recommended');
-
 export default tseslint.config(
   { ignores: ['dist'] },
   {
@@ -19,15 +15,10 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@tanstack/query': pluginQuery,
-      'prettier/prettier': eslintConfigPrettier,
     },
     rules: {
-      ...tseslint.configs.recommended.rules,
-      ...pluginQuery.configs['flat/recommended'],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      ...eslintPluginPrettierRecommended,
     },
   }
 );
